043e573f029854d9c68ced12f185c040da2cabb9
Lokii-98 <alokeshkumar1998@gmail.com>
2021-08-07 14:32:59 +0530

    depolyment error cleared

--- pages/api/GETserviceHandler.js
+++ pages/api/GETserviceHandler.js
@@ -1,3 +1,5 @@
-export default GETServiceHandler(){
-    return ''
-}
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+  res.status(200).json({ name: "John Doe" });
+};
